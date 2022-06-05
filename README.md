a136c3c483024323852affbcea15daeb70417be9
naif <nayef7138@gmail.com>
2022-06-05 14:19:43 +0300

    init files

---
+++ README.md
@@ -0,0 +1,6 @@
+# template for new html projects using vite and github pages
+
+## note
+
+1. change `package.json` **name** to your project name
+1. change `vite.config.ts` **base** to your project name
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-light': 'hsl(212, 45%, 89%)',
+        'primary-dark': 'hsl(218, 44%, 22%)',
+        'primary-base': 'hsl(220, 15%, 55%)',
+      },
+
+      fontFamily: {
+        qutfit: ["'Outfit'", 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
